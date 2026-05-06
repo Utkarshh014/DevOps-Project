ce61bf03ea2109354751122989fd5a8f69df4003
Utkarshh014 <shekharutkarsh75@gmail.com>
2026-05-06 15:07:54 +0530

    Configure Terraform remote backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "utkarsh-devops-tf-state-12345"
+    key    = "devops-prj/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
