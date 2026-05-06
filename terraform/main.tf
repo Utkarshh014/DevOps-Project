63e45a5b645a9d7d87e6cd30bc923e04552258ee
Utkarshh014 <shekharutkarsh75@gmail.com>
2026-05-06 16:47:02 +0530

    Add Application Load Balancer via Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,72 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
   ignore_public_acls  = true
   restrict_public_buckets = true
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "devops-alb-sg"
+  description = "ALB Security Group"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "ecs_from_alb" {
+  type                     = "ingress"
+  from_port                = 3000
+  to_port                  = 3000
+  protocol                 = "tcp"
+  security_group_id        = "sg-0807e7b3ce4f6c97c"
+  source_security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_lb" "app_alb" {
+  name               = "devops-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = ["subnet-0e7c40751f51eeb20", "subnet-0ed62f8291cd81ff5"]
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name        = "devops-app-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    timeout             = 60
+    interval            = 300
+    matcher             = "200,301,302"
+  }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "bucket_name" {
   value = aws_s3_bucket.app_bucket.bucket
 }
+
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
+
+output "alb_target_group_arn" {
+  value = aws_lb_target_group.app_tg.arn
+}
